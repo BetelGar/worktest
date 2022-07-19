a3d98a9538422081f9f182db0047f062351e2414
DESKTOP-D4P20FS\skype <sprkahxhalcl@naver.com>
2022-07-19 15:16:18 +0900

    first commit 아자아자 !!!!

---
+++ src/main/java/edu/hi/ex/SpringBootLentcarApplication.java
@@ -0,0 +1,13 @@
+package edu.hi.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLentcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLentcarApplication.class, args);
+	}
+
+}
